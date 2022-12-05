27d5c29d1f98f1932f779f4a1863e7a8fefe2105
ARegardin <91766685+ARegardin@users.noreply.github.com>
2022-12-05 20:17:54 +0100

    Add files via upload

--- modele.py
+++ modele.py
@@ -1,6 +1,7 @@
 import numpy as np
 from copy import copy
 import matplotlib.pyplot as plt
+import random
 
 class Strain:
     def __init__(self):
@@ -29,6 +30,8 @@ Rhalf = 0.1*R0  #moitié de saturation des ressources
 R = R0  #ressources (va évoluer dans le temps)
 Rstar = 1
 strains = []
+# new_strains = []
+winners = [] #Les alphas des meilleures strains pour chaque sélection (si tout va bien, dans le même ordre que celui donné par les curseurs des autres paramètres)
 Time_passed = 0       
 tau = 4       #hours = spore germination time
 tau_counter = 0
@@ -38,17 +41,31 @@ Time_rich = 0
 Time_starvation = 0
 T_sur = 200
 
+
 def survival_vg(time_starvation, souche):
     global T_sur
     nu = 1.1 - (2 * souche.c)
     beta = 3.1 - (4 * souche.c)
     return (np.exp(-(nu*time_starvation)**(beta)) - np.exp(-(nu*T_sur)**(beta)))/(1 - np.exp(-(nu*T_sur)**(beta)))
 
-def initialisation():
+""" def initialisation():
     a = Strain()
-    strains.append(a)
+    strains.append(a) """
 
-def main_step(n, dt, n_starv):
+# def main_step(n, dt, n_starv): #
+#     global Time_passed, R, tau_counter, Time_rich, Time_starvation
+#     i = 0
+#     while i < n * dt:
+#         if R > Rstar:
+#             rich_step(dt)
+#             i += dt
+#         else:
+#             starvation_step(n_starv * dt)
+#             i += n_starv * dt
+#             R = 10**8
+#             tau_counter = 0
+
+def main_step(n, dt, n_starv): #
     global Time_passed, R, tau_counter, Time_rich, Time_starvation
     i = 0
     while i < n * dt:
@@ -56,17 +73,23 @@ def main_step(n, dt, n_starv):
             rich_step(dt)
             i += dt
         else:
-            starvation_step(n_starv * dt)
+            #normale_starvation()
+            starvation_step(n_starv * dt) #Remplacer par fonction répartition - "the growth phase starts with the arrival of a pulse of ressources"
             i += n_starv * dt
-            R = 10**8
+            R = 10**8 #Pulse of resources
             tau_counter = 0
+
+def normale_starvation(sigma):
+    #return un temps de starvation -> voir temps moyen de starvation
+
+    pass
 
 def starvation_step(t):
     global Time_passed, Time_starvation, Time_rich
 
     #stockage
     state = {"Time_passed" : Time_passed, "R" : R, "strains" : copyStrains(strains)}
-    data.append(state)
+    ### data.append(state)
 
     Time_rich = 0
 
@@ -135,9 +158,99 @@ def rich_step(dt):
 
 
 
-initialisation() #Main
+""" initialisation() #Main
 main_step(400, 0.5, 4)
+Pop1R_plot = [[],[],[],[]] """
 Pop1R_plot = [[],[],[],[]]
+
+def Normale_density(x, mu, sigma):
+    return (np.pi*sigma) * np.exp(-0.5*((x-mu)/sigma)**2)
+
+def Normale_starvationtime_randomselection(parameters_mu_sigma, step=1):                  #(OLD : domain = borne sup (borne inf = 0), size ->~= precision)
+    mu = parameters_mu_sigma[0]
+    sigma = parameters_mu_sigma[1]
+    
+    prob_table = []
+    x=0
+    while prob_table[-1]>=0.001:
+        prob_table.append(Normale_density(x, mu, sigma))
+        x+=step
+
+    i = random.randint(0, len(prob_table))
+    return prob_table[i]
+
+
+def Exp_starvationtime(lambdaST, size, domain):
+    pass
+
+
+def Simulation_and_Selection(parameters, density=Normale_starvationtime_randomselection):   #Lance toute la simulation pour une densité de proba dépendant des params donnés,
+                                                                                                     #et sélectionne le meilleur génotype
+    global strains
+    global winners
+    
+    graph_return = []
+    tmax = 2*108
+    dt = 1 #h
+
+    main_step(tmax, dt, density(parameters))
+
+    counting_survivors = []
+    for s in range(len(strains)):
+        counting_survivors.append(data[-1]["strains"][s].spores + data[-1]["strains"][s].vg)
+    
+    best = 0
+    for i in range(len(counting_survivors)):
+        if counting_survivors[best]<=counting_survivors[i]:
+            best = i
+
+    winners.append(strains[best].alpha)
+    pass
+
+
+def plot_repartition(parameters, density='Normale'):      # parameters est un tableau car les différentes lois n'ont pas besoin du même nombre de variables
+    global winners
+    global strains
+
+    #tmax : t = 2 × 108 h
+    """ tmax = 2*108
+    dt = 1 #h """
+
+    new_strains = [] #Important, pour ne pas avoir à recréer les "fiches d'identités" des strains (en voulant éviter les bugs de non-effacement des tailles de populations) (oui ça pourrait être global mais jpp)
+
+    strains_id = np.linspace(0, 1000, 1001)
+    alphas = 1/strains_id
+    for i in range(len(strains_id)):
+        strain = Strain()
+        strain.alpha = alphas[strains_id[i]]
+        new_strains.append(strain)
+
+    if density == 'Normale' :
+        mus = np.linspace(0, 40, 41)*10   #de 0 à 400 heures (voir figure 4.C)
+        sigmas = np.linspace(0, 100, 6)
+        best_genotypes_sigma = []
+        for sigma in sigmas:
+            for mu in mus:
+                winners = []
+                parameters_proba = [mu, sigma]
+                strains = copy(new_strains)
+                Simulation_and_Selection(parameters_proba)
+                best_genotypes_mu = winners
+            best_genotypes_sigma.append(best_genotypes_mu)
+        
+        ordered_parameters = [] #On range les mus et sigmas selon la même structure que best_genotypes_sigma pour pouvoir tracer les courbes
+        for i in range(len(sigmas)):
+            subtable=[]
+            for j in range(len(mus)):
+                subtable.append([sigmas[i], mus[j]])
+            ordered_parameters.append(subtable)
+        # Il n'y a plus qu'à faire correspondre [sigma, mu==Mean_starvation] <-> [liste de genotypes]
+
+        pass
+
+    pass
+
+
 
 def plot_popstrain(strain_name):
     strains_list = data[0]["strains"]
@@ -152,7 +265,6 @@ def plot_popstrain(strain_name):
         Pop1R_plot[2].append(data[i]["strains"][index_strain].vg)
         Pop1R_plot[3].append(data[i]["strains"][index_strain].spores)
         t.append(data[i]["Time_passed"])
-
     
     
     #plt.scatter(t, Pop1R_plot[0], label='Ressources')
